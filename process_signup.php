e9c8d1237d3853df144c24bdcd7c4b62892739eb
SergioFrasco <128463412+SergioFrasco@users.noreply.github.com>
2023-03-27 15:37:31 +0200

    PHP commit
    Small changes made to php

--- process_signup.php
+++ process_signup.php
@@ -1,7 +1,7 @@
 <?php
 // Retrieve the email and password values from the form submission
-$email = $_POST['email'];
-$password = $_POST['password'];
+$user_email = $_POST['email'];
+$user_password = $_POST['password'];
 
 // Connect to the MySQL database
 $servername = "localhost";
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 }
 
 // Insert the email and password values into the 'users' table in your database
-$sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+$sql = "INSERT INTO users (email, password) VALUES ('$user_email', '$user_password')";
 if ($conn->query($sql) === FALSE) {
 	echo "Error: " . $sql . "<br>" . $conn->error;
 }
